81d49a632e7967da1c410e0b99d3dfc2d3db3b70
Célio <celiofagundes30@gmail.com>
2022-07-29 17:08:57 -0300

    feat: added api fetch

---
+++ lib/api.ts
@@ -0,0 +1,18 @@
+import { getAuth } from 'firebase/auth'
+
+export const fetchMedias = async () => {
+  const token = await getAuth().currentUser?.getIdToken()
+  const response = await fetch('http://localhost:3000/medias', {
+    headers: {
+      authToken: token ? token : '',
+    },
+  })
+  if (!response.ok) {
+    if (response.status === 401) {
+      throw new Error('Não autorizado')
+    }
+    throw new Error('Error ao carregar dados')
+  }
+  const data = await response.json()
+  return data
+}
